aa8f04201022ee01d35d96022ad982453dd8f296
TuuuNya <song@secbox.cn>
2025-01-13 00:16:59 +0800

    feat: update serve command help text to clarify supported proxy server type

--- cmd/serve.go
+++ cmd/serve.go
@@ -21,7 +21,7 @@ var ServeCmd = &cobra.Command{
 }
 
 func init() {
-	ServeCmd.Flags().StringVarP(&ServerArgs.Type, "type", "t", "socks5", "Proxy server type")
+	ServeCmd.Flags().StringVarP(&ServerArgs.Type, "type", "t", "socks5", "Proxy server type, currently only supports socks5")
 	ServeCmd.Flags().IntVarP(&ServerArgs.Port, "port", "p", 7777, "Port to listen on")
 	ServeCmd.Flags().Duration("max_delay", 10*time.Second, "Maximum delay for proxy server")
 	ServeCmd.Flags().IntVarP(&ServerArgs.CheckPoolSize, "check_pool_size", "c", 100000, "Size of the pool to check proxies")
